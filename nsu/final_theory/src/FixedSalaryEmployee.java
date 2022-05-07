292620aa8234f77761f66d78b08425a946699ec3
humam-hossain <humam.hossain.e@gmail.com>
2022-05-08 01:40:47 +0600

    updated on Sun, May  8, 2022  1:40:46 AM

---
+++ nsu/final_theory/src/FixedSalaryEmployee.java
@@ -0,0 +1,13 @@
+public class FixedSalaryEmployee extends Employee {
+    double basicSalary;
+    
+    FixedSalaryEmployee() {
+        super();
+        //TODO Auto-generated constructor stub
+    }  
+    
+    @Override
+    double calculateSalary(){
+        
+    }
+}
---
+++ nsu/final_theory/src/HourlyRateEmployee.java
@@ -0,0 +1,14 @@
+public class HourlyRateEmployee extends Employee{
+    double perHourRate;
+    double totalHours;
+    
+    HourlyRateEmployee() {
+        super();
+        //TODO Auto-generated constructor stub
+    }
+
+    @Override
+    double calculateSalary(){
+        
+    }
+}
